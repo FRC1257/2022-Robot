c47f6d4b572a3db2da4f94579875a1b043babd6d
Akash Dubey <69588353+AkeBoss-tech@users.noreply.github.com>
2022-01-24 15:41:32 -0500

    Created subsystem file

---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,37 @@
+package frc.robot.subsystems;
+
+
+public class Climber extends SnailSubsystem {
+    public enum States {
+
+    }
+
+    public Climber() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void displayShuffleboard() {
+
+    }
+
+    @Override
+    public void tuningInit() {
+
+    }
+
+    @Override
+    public void tuningPeriodic() {
+
+    }
+
+    public State getState() {
+        return state;
+    }
+    
+}
