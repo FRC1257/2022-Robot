40b3c6735c32c6619382b9366efea2df1662f5e4
Akash Dubey <69588353+AkeBoss-tech@users.noreply.github.com>
2022-01-24 15:47:53 -0500

    created climber subsystem file

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,18 +1,50 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import static frc.robot.Constants.ElectricalLayout.*;
+import static frc.robot.Constants.climber.*;
+import static frc.robot.Constants.NEO_CURRENT_LIMIT;
 
 public class Climber extends SnailSubsystem {
-    public enum States {
 
+    private CANSparkMax climberMotor;
+    private CANSparkMax climberFollowerMotor;
+
+    public enum State {
+        MANUAL
     }
 
-    public Climber() {
+    State state = State.MANUAL;
+    private double speed = 0;
 
+    public Climber() {
+        climberMotor = new CANSparkMax(climber_PRIMARY_ID, MotorType.kBrushless);
+        climberMotor.restoreFactoryDefaults();
+        climberMotor.setIdleMode(IdleMode.kBrake);
+        climberMotor.setSmartCurrentLimit(NEO_CURRENT_LIMIT);
+
+        climberFollowerMotor = new CANSparkMax(climber_FOLLOWER_ID, MotorType.kBrushless);
+        climberFollowerMotor.restoreFactoryDefaults();
+        climberFollowerMotor.setIdleMode(IdleMode.kBrake);
+        climberFollowerMotor.setSmartCurrentLimit(NEO_CURRENT_LIMIT);
+        climberFollowerMotor.follow(climberMotor, false); // following
     }
 
     @Override
     public void update() {
+        switch(state) {
+            case MANUAL:
+                elevatorMotor.set(speed);
+                break;
+    }
+    }
 
+    public void manualControl(double speed){
+        this.speed = speed;
+        state = State.MANUAL;
     }
 
     @Override
