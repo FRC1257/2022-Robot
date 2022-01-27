31779360acf63a50b5a096c9577369a026009429
Jason-Bak <56934929+Jason-Bak@users.noreply.github.com>
2022-01-27 15:15:54 -0500

    Create RobotIntakeNeutralCommand.java

---
+++ src/main/java/frc/robot/commands/RobotIntakeNeutralCommand.java
@@ -0,0 +1,33 @@
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.RollerIntake;
+
+public class RollerIntakeNeutralCommand extends CommandBase {
+
+    private RollerIntake rollerIntake;
+
+    public RollerIntakeIntakeCommand(RollerIntake rollerIntake) {
+        this.rollerIntake = rollerIntake;
+
+        addRequirements(rollerIntake);
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+        rollerIntake.neutral();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
