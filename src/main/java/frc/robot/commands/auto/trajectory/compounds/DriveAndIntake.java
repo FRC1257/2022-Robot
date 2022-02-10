0859a5d3452a8f03e68cfb89de9726fa95b95c7d
thatcanasian <95885775+thatcanasian@users.noreply.github.com>
2022-02-10 16:21:07 -0500

    edited drive intake command to work with this years files

--- src/main/java/frc/robot/commands/auto/trajectory/compounds/DriveAndIntake.java
+++ src/main/java/frc/robot/commands/auto/trajectory/compounds/DriveAndIntake.java
@@ -2,17 +2,17 @@ package frc.robot.commands.auto.segmented;
 
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import frc.robot.commands.drivetrain.DriveDistanceCommand;
-import frc.robot.commands.Conveyor.ConveyorEjectCommand;
-import frc.robot.commands.shooter.ShooterPIDCommand;
+import frc.robot.commands.conveyor.ConveyorEjectCommand;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.Conveyor;
+import frc.robot.subsystems.Indexer;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 
 import static frc.robot.Constants.Autonomous.CONVEYOR_DUMP_TIME;
 
 public class DriveAndIntake extends ParallelDeadlineGroup {
 
-    public DriveAndIntake(Drivetrain drivetrain, Indexer indexer, Intake intake, double dist) {
+    public DriveAndIntake(Drivetrain drivetrain, Intake intake, double dist) {
         addCommands(
             new DriveDistance(drivetrain, dist),
             //new Intake(/*syntax blah blah blah*/)
