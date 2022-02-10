3dc1d7dbe26d973f9d868e86859a3aaef6ca323a
thatcanasian <95885775+thatcanasian@users.noreply.github.com>
2022-02-10 16:19:47 -0500

    changed imports in drive/intake

--- src/main/java/frc/robot/commands/auto/trajectory/compounds/DriveAndIntake.java
+++ src/main/java/frc/robot/commands/auto/trajectory/compounds/DriveAndIntake.java
@@ -2,14 +2,13 @@ package frc.robot.commands.auto.segmented;
 
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import frc.robot.commands.drivetrain.DriveDistanceCommand;
-import frc.robot.commands.indexer.IndexerShootCommand;
+import frc.robot.commands.Conveyor.ConveyorEjectCommand;
 import frc.robot.commands.shooter.ShooterPIDCommand;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.Indexer;
-import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Conveyor;
 import frc.robot.subsystems.Shooter;
 
-import static frc.robot.Constants.Autonomous.INDEXER_DUMP_TIME;
+import static frc.robot.Constants.Autonomous.CONVEYOR_DUMP_TIME;
 
 public class DriveAndIntake extends ParallelDeadlineGroup {
 
